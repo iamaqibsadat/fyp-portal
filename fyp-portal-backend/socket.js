771cb17e18d8ad4dd824292b4c7e0db5ccb630ff
elyaskhan-web <75215842+elyaskhan-web@users.noreply.github.com>
2024-07-30 10:26:52 +0500

    Update socket.js

--- fyp-portal-backend/socket.js
+++ fyp-portal-backend/socket.js
@@ -4,7 +4,7 @@ module.exports = {
   init: httpServer => {
     io = require('socket.io')(httpServer, {
       cors: {
-        origin: 'https://fyp-portal-backend.onrender.com',
+        origin: 'https://fyp-portal-frontend.onrender.com',
         methods: ['GET', 'POST']
       }
     });
