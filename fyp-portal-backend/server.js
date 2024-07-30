4a2ef9a551f159abee80f8c7d34bf905ada64050
elyaskhan-web <75215842+elyaskhan-web@users.noreply.github.com>
2024-07-30 12:34:44 +0500

    Update server.js

--- fyp-portal-backend/server.js
+++ fyp-portal-backend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const socket = require('./socket');
 const app = require('./app');
 
@@ -30,14 +27,6 @@ io.on('connection', (socket) => {
   socket.on('message', (message) => {
     io.emit('message', message);
   });
-});
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'fyp-portal-frontend/build')));
-
-// Catch-all route to serve index.html for SPA routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'fyp-portal-frontend/build', 'index.html'));
 });
 
 // Start the server
